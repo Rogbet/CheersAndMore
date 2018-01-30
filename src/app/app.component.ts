4a78da7ddd115bf722681208b779da20e086967a
rogbet <rogbet@hotmail.com>
2018-01-30 02:59:00 -0600

    Add Toolbar and menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,9 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 export class AppComponent implements OnInit{
   user: Observable<firebase.User>;
   items: Observable<any[]>;
+  reason = '';
+  @ViewChild('sidenav') sidenav: MatSidenav;
+
   constructor(public afAuth: AngularFireAuth,public db: AngularFirestore) {
       
     }
@@ -22,5 +26,10 @@ export class AppComponent implements OnInit{
       this.afAuth.auth.signInAnonymously();
       this.user = this.afAuth.authState;
       this.items = this.db.collection('/items').valueChanges();
+    }
+
+    close(reason: string) {
+      this.reason = reason;
+      this.sidenav.close();
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule }   from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatButtonToggleModule, MatIconModule, MatCheckboxModule, MatSidenavModule,MatListModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -20,11 +21,17 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
     BrowserAnimationsModule,
     FormsModule,
     MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
     MatCheckboxModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
